ee483b0262ca6422bbf2171dd8be605bde8f3012
Kevin <kudy@upcsite.net>
2011-09-07 17:34:52 -0600

    initial commit

---
+++ image_array.php
@@ -0,0 +1,7 @@
+<?php
+$imageArray = array(
+	0 => array("address" => "images/image1.jpg", "alt_text" => "Image 1"),
+	1 => array("address" => "images/image2.jpg", "alt_text" => "Image 2"),
+	2 => array("address" => "images/image1.jpg", "alt_text" => "Image 3")
+);
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//Include the file which contains the Image array
+include "image_array.php";
+
+//Select the index number of one random array entry
+$randomImageNumber = array_rand($imageArray, 1);
+
+//Print to the screen an HTML image element with attributes pulled from the randomly selected array entry
+echo "<img src='" . $imageArray[$randomImageNumber]['address'] . "' alt='" . imageArray[$randomImageNumber]['alt_text'] . "' />\n";
+?>
