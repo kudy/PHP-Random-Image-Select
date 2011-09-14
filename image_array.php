c240537a75778c444cef594c02f77ce5a645330c
Kevin <kudy@upcsite.net>
2011-09-14 17:46:44 -0600

    Fix type in array

--- image_array.php
+++ image_array.php
@@ -2,6 +2,6 @@
 $imageArray = array(
 	0 => array("address" => "images/image1.jpg", "alt_text" => "Image 1"),
 	1 => array("address" => "images/image2.jpg", "alt_text" => "Image 2"),
-	2 => array("address" => "images/image1.jpg", "alt_text" => "Image 3")
+	2 => array("address" => "images/image3.jpg", "alt_text" => "Image 3")
 );
 ?>
